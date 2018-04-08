c81dcd28b472ed76f06ada3fc9845ac7729b048a
thuitafaith <thuitamuthoni15@gmail.com>
2018-04-08 13:00:35 +0300

    code snippet added to the config file for configurations

--- app/views.py
+++ app/views.py
@@ -6,3 +6,7 @@ from app import app
 def index():
     message = 'News Highlight'
     return render_template('index.html', message = message)
+
+@app.route('/news/<news_everything>')
+def news(news_everything):
+    return render_template('news.html', everything = news_everything)
